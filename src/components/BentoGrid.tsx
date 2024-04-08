44e1b17dbc34bb79af1923f020ed4854dfc85da5
Abhay Singh Rathore <abhaysinghr516@gmail.com>
2024-04-08 15:13:15 +0530

    Update Bento Grid components and documentation

---
+++ src/components/BentoGrid.tsx
@@ -0,0 +1,153 @@
+export const BentoGrid1: React.FC = () => (
+  <div className="grid grid-cols-4 grid-rows-3 gap-2 bg-black p-5">
+    <div className="col-span-1 row-span-2 flex h-60 items-center justify-center rounded-xl bg-yellow-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-xl bg-green-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-2 row-span-2 flex items-center justify-center rounded-xl bg-purple-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-2 row-span-1 flex items-center justify-center rounded-xl bg-red-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-lg bg-indigo-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+  </div>
+);
+
+export const BentoGrid2: React.FC = () => (
+  <div className="grid grid-cols-4 grid-rows-4 gap-2 bg-black p-5">
+    <div className="col-span-1 row-span-2 flex h-60 items-center justify-center rounded-xl bg-yellow-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-green-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-purple-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-xl bg-purple-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-2 row-span-2 flex items-center justify-center rounded-xl bg-red-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-lg bg-indigo-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-2 row-span-1 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+  </div>
+);
+
+export const BentoGrid3: React.FC = () => (
+  <div className="grid grid-cols-3 grid-rows-6 gap-2 bg-black p-5">
+    <div className="col-span-1 row-span-3 flex h-60 items-center justify-center rounded-xl bg-yellow-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-green-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-3 flex items-center justify-center rounded-xl bg-purple-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-purple-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-red-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-3 flex items-center justify-center rounded-lg bg-indigo-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-2 row-span-1 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+  </div>
+);
+
+export const BentoGrid4: React.FC = () => (
+  <div className="grid grid-cols-5 grid-rows-4 gap-2 bg-black p-5">
+    <div className="col-span-1 row-span-1 flex h-24 items-center justify-center rounded-xl bg-yellow-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-green-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-purple-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-2 row-span-1 flex items-center justify-center rounded-xl bg-purple-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-xl bg-red-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-3 row-span-2 flex items-center justify-center rounded-lg bg-indigo-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-3 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-2 row-span-1 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+  </div>
+);
+
+export const BentoGrid5: React.FC = () => (
+  <div className="grid grid-cols-3 grid-rows-5 gap-2 bg-black p-5">
+    <div className="col-span-1 row-span-1 flex h-24 items-center justify-center rounded-xl bg-yellow-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-xl bg-green-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-3 flex items-center justify-center rounded-xl bg-purple-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-purple-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-xl bg-red-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-lg bg-indigo-100">
+      <div className="text-center text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-2 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+    <div className="col-span-1 row-span-1 flex items-center justify-center rounded-xl bg-orange-100">
+      <div className="text-xl font-medium">Content</div>
+    </div>
+  </div>
+);
