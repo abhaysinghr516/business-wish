879d0e29612085f94b3dae770c39e3d24f84e6ad
Abhay Singh Rathore <abhaysinghr516@gmail.com>
2024-06-05 14:03:42 +0530

    chore: Update robots.txt disallow paths

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
             {
                 userAgent: "*",
                 allow: "/",
-                disallow: ["/admin", "/privacy"],
+                disallow: ["/components", "/templates", "/test"],
             },
         ],
         sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,11 @@
-import { ComponentData } from "../data/componentsData";
+import { componentsData } from "../data/componentsData";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const componentsData: ComponentData[] = [
-        {
-            href: "/components/404",
-            title: "Error404",
-            components: "2",
-        },
-        {
-            href: "/components/accordion",
-            title: "Accordion",
-            components: "2",
-        },
-    ];
 
-    const componentEntries: MetadataRoute.Sitemap = componentsData.map(({ href }) => ({
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}${href}`,
+    const componentEntries: MetadataRoute.Sitemap = componentsData.map((component, index) => ({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}${component.href}`,
+        lastModified: new Date(),
     }));
 
     return [
