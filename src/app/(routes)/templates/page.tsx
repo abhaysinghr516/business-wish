06e21e7769a1d1a94b64e26a76549f6aece04ccc
Abhay Singh Rathore <abhaysinghr516@gmail.com>
2024-07-12 19:51:17 +0530

    Refactor page template and update styles

--- src/app/(routes)/templates/page.tsx
+++ src/app/(routes)/templates/page.tsx
@@ -8,23 +8,23 @@ const Page = () => {
   return (
     <>
       <Navbar />
-      <main className="relative flex flex-grow items-center justify-center py-20 px-4 sm:px-6 lg:px-8 bg-gray-950 min-h-screen overflow-hidden">
+      <main className="relative flex flex-grow items-center justify-center py-10 px-4 sm:px-6 lg:px-8 bg-gray-950 min-h-screen overflow-hidden">
         {/* Animated background shapes */}
         <div className="absolute inset-0 overflow-hidden">
-          <div className="animate-float absolute -top-16 -left-16 w-44 h-44 bg-purple-400 rounded-full mix-blend-lighten filter blur-sm opacity-80"></div>
-          <div className="animate-float absolute top-32 -right-16 w-44 h-44 bg-yellow-300 rounded-full mix-blend-lighten filter blur-sm opacity-80 animation-delay-2000"></div>
-          <div className="animate-float absolute -bottom-16 left-16 w-44 h-44 bg-pink-300 rounded-full mix-blend-lighten filter blur-sm opacity-80 animation-delay-4000"></div>
+          <div className="animate-float absolute -top-16 -left-16 w-24 h-24 sm:w-44 sm:h-44 bg-purple-400 rounded-full mix-blend-lighten filter blur-sm opacity-80"></div>
+          <div className="animate-float absolute top-24 sm:top-32 -right-16 w-24 h-24 sm:w-44 sm:h-44 bg-yellow-300 rounded-full mix-blend-lighten filter blur-sm opacity-80 animation-delay-2000"></div>
+          <div className="animate-float absolute -bottom-16 left-16 w-24 h-24 sm:w-44 sm:h-44 bg-pink-300 rounded-full mix-blend-lighten filter blur-sm opacity-80 animation-delay-4000"></div>
         </div>
 
-        <div className="max-w-4xl w-full text-center space-y-8 relative z-10">
-          <h1 className="text-5xl md:text-7xl font-extrabold text-gray-100 mb-4">
+        <div className="max-w-4xl w-full text-center space-y-4 sm:space-y-8 relative z-10">
+          <h1 className="text-3xl sm:text-5xl md:text-7xl font-extrabold text-gray-100 mb-2 sm:mb-4">
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-400 to-purple-500">
               Exciting Templates
             </span>
             <br />
             Coming Soon!
           </h1>
-          <p className="text-xl md:text-2xl text-gray-300 mb-12 max-w-3xl mx-auto leading-relaxed">
+          <p className="text-lg sm:text-xl text-gray-300 mb-6 sm:mb-12 max-w-md sm:max-w-3xl mx-auto leading-relaxed">
             I'm crafting a stunning, versatile template to elevate your
             projects. I'm putting the finishing touches on a design that will
             transform your ideas into reality.
