106b8cb6fd20c4d81421ab253794771ae6b9fbc7
Abhay Singh Rathore <abhaysinghr516@gmail.com>
2024-07-22 22:54:45 +0530

    chore: Refactor ComponentPage and update imports

--- src/app/(routes)/components/[component]/page.tsx
+++ src/app/(routes)/components/[component]/page.tsx
@@ -1,107 +1,22 @@
-"use client";
-import { usePathname } from "next/navigation";
-import {
-  Error404Docs,
-  AccordionDocs,
-  AlertDocs,
-  AvatarDocs,
-  BadgeDocs,
-  BannerDocs,
-  BentoDocs,
-  BlogListDocs,
-  BottomNavigationDocs,
-  BreadcrumbsDocs,
-  ButtonDocs,
-  CtaDocs,
-  CardDocs,
-  DividerDocs,
-  DropdownDocs,
-  FeaturesDocs,
-  FooterDocs,
-  HeroDocs,
-  HeaderDocs,
-  LoaderDocs,
-  ModalDocs,
-  PaginationDocs,
-  SettingsDocs,
-  SidebarDocs,
-  SkeletonDocs,
-  TabsDocs,
-  TestimonialsDocs,
-} from "../../../docs/index";
+import { Metadata } from "next";
+import { componentsData } from "../../../../data/componentsData";
+import ComponentPage from "@/app/components/Pages/Component/ComponentPage";
 
-// src/components/ComponentPage.tsx
-import { ComponentData, componentsData } from "../../../../data/componentsData";
-import Navbar from "@/app/components/Navbar";
-import Sidebar from "@/app/components/Sidebar";
+export function generateMetadata({
+  params,
+}: {
+  params: { component: string };
+}): Metadata {
+  const pathname = `/components/${params.component}`;
+  const componentData = componentsData.find((data) => data.href === pathname);
 
-type ComponentKey =
-  | "Error404"
-  | "Accordion"
-  | "Alert"
-  | "Avatar"
-  | "Badge"
-  | "Banner"
-  | "Bento"
-  | "BlogList"
-  | "BottomNavigation"
-  | "Breadcrumbs"
-  | "Button"
-  | "CTA"
-  | "Card"
-  | "Divider"
-  | "Dropdown"
-  | "Features"
-  | "Footer"
-  | "Hero"
-  | "Header"
-  | "Loader"
-  | "Modal"
-  | "Pagination"
-  | "Settings"
-  | "Sidebar"
-  | "Skeleton"
-  | "Tabs"
-  | "Testimonials";
+  return {
+    title: componentData?.title || "Component",
+    description: componentData?.description || "Component description",
+    keywords: componentData?.keywords || "Component keywords",
+  };
+}
 
-const components = {
-  Error404: Error404Docs,
-  Accordion: AccordionDocs,
-  Alert: AlertDocs,
-  Avatar: AvatarDocs,
-  Badge: BadgeDocs,
-  Banner: BannerDocs,
-  Bento: BentoDocs,
-  BlogList: BlogListDocs,
-  BottomNavigation: BottomNavigationDocs,
-  Breadcrumbs: BreadcrumbsDocs,
-  Button: ButtonDocs,
-  CTA: CtaDocs,
-  Card: CardDocs,
-  Divider: DividerDocs,
-  Dropdown: DropdownDocs,
-  Features: FeaturesDocs,
-  Footer: FooterDocs,
-  Hero: HeroDocs,
-  Header: HeaderDocs,
-  Loader: LoaderDocs,
-  Modal: ModalDocs,
-  Pagination: PaginationDocs,
-  Settings: SettingsDocs,
-  Sidebar: SidebarDocs,
-  Skeleton: SkeletonDocs,
-  Tabs: TabsDocs,
-  Testimonials: TestimonialsDocs,
-};
-
-export default function ComponentPage() {
-  const pathname = usePathname();
-  const componentData: ComponentData | undefined = componentsData.find(
-    (data) => data.href === pathname
-  );
-  const Component = componentData
-    ? components[componentData.title as ComponentKey]
-    : null;
-
-  return <div className=" ">{Component ? <Component /> : null}</div>;
+export default function Page() {
+  return <ComponentPage />;
 }
