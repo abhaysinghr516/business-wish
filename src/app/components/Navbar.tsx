6d7d6b3009839767c1cece31960b0eb5d1cd0424
Abhay Singh Rathore <abhaysinghr516@gmail.com>
2024-09-22 20:02:23 +0530

    Refactor Navbar component to add collapsible mobile menu and update styling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import React, { useState } from "react";
 import Link from "next/link";
-import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import ThemeToggle from "./theme-switch";
 import { FiGithub } from "react-icons/fi";
 import { FaXTwitter } from "react-icons/fa6";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { DialogTitle } from "@/components/ui/dialog";
+import DocsMenu from "./docs-menu";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,11 +30,8 @@ const Navbar = () => {
         <Link href="/" className="ml-2 text-xl font-bold text-white">
           <img src="/logo2.png" alt="logo" className="h-5" />
         </Link>
-        <div
-          className={`items-center space-x-6 md:flex ${
-            isOpen ? "block" : "hidden md:flex"
-          }`}
-        >
+
+        <div className="hidden md:flex items-center space-x-6">
           <Link
             href="/docs/components/404"
             className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:scale-x-0 after:bg-gray-900 after:transition-all after:duration-300 hover:text-gray-900 hover:after:scale-x-100"
@@ -43,21 +50,50 @@ const Navbar = () => {
           <Link href="https://x.com/abhaysinghr1">
             <FaXTwitter className="h-4 w-4" />
           </Link>
-          {/* <ThemeToggle /> */}
         </div>
-        <div className="md:hidden">
-          <button
-            id="menu-toggle"
-            onClick={toggleMenu}
-            className="mr-2 text-gray-300 hover:text-white focus:outline-none"
-          >
-            {isOpen ? (
-              <FaTimes className="h-6 w-6" />
-            ) : (
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon" className="md:hidden flex">
               <FaBars className="h-6 w-6" />
-            )}
-          </button>
-        </div>
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            className="w-[300px] sm:w-[400px] flex flex-col gap-4 px-0"
+            side="left"
+          >
+            <DialogTitle className="sr-only">Menu</DialogTitle>
+            <SheetHeader>
+              <SheetClose className="px-5" asChild>
+                <Link href="/" className="ml-2 text-xl font-bold text-white">
+                  <img src="/logo2.png" alt="logo" className="h-5" />
+                </Link>
+              </SheetClose>
+            </SheetHeader>
+            <ScrollArea className="flex-grow">
+              <div className="flex flex-col gap-4 p-6">
+                <Link href="/docs/components/404" className="text-sm">
+                  Components
+                </Link>
+                <Link href="/templates" className="text-sm">
+                  Templates
+                </Link>
+                <Link
+                  href="https://github.com/abhaysinghr516/business-wish"
+                  className="text-sm"
+                >
+                  GitHub
+                </Link>
+                <Link href="https://x.com/abhaysinghr1" className="text-sm">
+                  Twitter
+                </Link>
+              </div>
+              <div className="px-6">
+                <DocsMenu isSheet />
+              </div>
+            </ScrollArea>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   );
